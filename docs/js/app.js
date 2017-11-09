900d6efb14caaa03231b60f05b232f60439478db
eduardlio <liao.edward@gmail.com>
2017-11-09 21:56:18 +1000

    docs ain't working lmao

---
+++ docs/js/app.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    var count = 0;
+    var parts = ['part-project', 'part_a', 'part_b', 'part_c', 'portfolio_part'];
+
+    function hideAllBut(num) {
+        console.log("hiding all but: " + num);
+        if (num < count) {
+            console.log("sorting from num more than count");
+            $('.' + parts[count]).hide( );
+            count = num;
+            $('.' + parts[count]).css('display', 'inline-flex');
+            // animate curren to go right
+            // animate next come in from left to right
+            // set curent = num
+        }
+        if (num > count) {
+            console.log("sorting from num more than count");
+            $('.' + parts[count]).hide( );
+            count = num;
+            $('.' + parts[count]).css('display', 'inline-flex');
+            // animate current go screenLeft
+            // animate next come in form right to left
+            // set current = num
+        }
+    }
+    $(".move").on("click", "#move-next", function () {
+        console.log("click");
+        if( (count+1) > parts.length-1 ){
+            hideAllBut(0);
+        }else{
+            hideAllBut(count + 1);
+        }
+    });
+    $(".move").on("click", "#move-prev", function () {
+        console.log("click");
+        if( (count-1) < 0) {
+            hideAllBut(parts.length-1);
+        }else{
+            hideAllBut(count - 1);
+        }
+    });
+});
